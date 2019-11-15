cfa2260daddb1e791a3a7600730b387b2f48d746
zakariaBoukernafa <zakaria0697@gmail.com>
2019-11-15 22:08:58 +0100

    websocket implementation

--- src/lambda/http/getImages.ts
+++ src/lambda/http/getImages.ts
@@ -8,6 +8,7 @@ const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
   console.log('Caller event', event)
   const groupId = event.pathParameters.groupId
   const validGroupId = await groupExists(groupId)
@@ -24,16 +25,15 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }
   }
 
-  // TODO: Create an image
   const images = await getImagesPerGroup(groupId)
 
   return {
-    statusCode: 200,
+    statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-        items:images
+      items: images
     })
   }
 }
@@ -51,16 +51,16 @@ async function groupExists(groupId: string) {
   console.log('Get group: ', result)
   return !!result.Item
 }
-async function getImagesPerGroup(groupId:string){
-    const result = await docClient.query({
-        TableName:imagesTable,
-        KeyConditionExpression:'groupId = :groupId',
-        ExpressionAttributeValues:{
-            ':groupId':groupId
-        },
-        ScanIndexForward:false
 
-    }).promise()
-    return result.Items
+async function getImagesPerGroup(groupId: string) {
+  const result = await docClient.query({
+    TableName: imagesTable,
+    KeyConditionExpression: 'groupId = :groupId',
+    ExpressionAttributeValues: {
+      ':groupId': groupId
+    },
+    ScanIndexForward: false
+  }).promise()
 
+  return result.Items
 }
